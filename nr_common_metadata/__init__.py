86eb903342f4d304764605c1b25ddb219977974a
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-07-29 13:58:27 +0200

    add NRAuthority definition

---
+++ nr_common_metadata/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022 CESNET
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from .ext import NRCommonMetadataExt
+
+__version__ = "1.0.0"
+
+__all__ = ('__version__', 'NRCommonMetadataExt')
